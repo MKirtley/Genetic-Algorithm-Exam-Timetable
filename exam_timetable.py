70c7c0094ddb932cfc50a75165a0d6fef0a177f8
Matt Kirtley <54330969+MKirtley@users.noreply.github.com>
2022-12-13 17:34:49 +0000

    All hard constraints passed.
    Still have to implement mutation.

--- exam_timetable.py
+++ exam_timetable.py
@@ -1,9 +1,7 @@
 import csv
 from collections import namedtuple, Counter, defaultdict
 import random
-import numpy as np
 from copy import deepcopy
-import pandas as pd
 
 
 # Global Variables
@@ -105,9 +103,16 @@ def generate_exam_room(units, tutors):
     """
     Selecting a random unit and tutor and placing them in a random classroom.
     """
-    # Select a random unit and tutor for the morning and afternoon sessions.
-    morning_unit, morning_tutor = random.choice(units), random.choice(tutors)
-    afternoon_unit, afternoon_tutor = random.choice(units), random.choice(tutors)
+
+    available_units = units.copy()
+    # Select a random unit and tutor for the morning session
+    morning_unit, morning_tutor = random.choice(available_units), random.choice(tutors)
+
+    # Remove the morning unit from the list of available units
+    available_units.remove(morning_unit)
+
+    # Select a random unit and tutor for the morning session
+    afternoon_unit, afternoon_tutor = random.choice(available_units), random.choice(tutors)
 
     # Select a random classroom.
     room_name = random.choice(CLASSROOMS)
@@ -121,35 +126,48 @@ def generate_exam_room(units, tutors):
         tutor_afternoon=afternoon_tutor
     )
 
+
+
+
 def generate_population(population_size, units, tutors):
     """
     Generate a random population that fulfils all the hard constraints.
     :return: A new population. 
     """
     new_population = []
+
     # Create a solution for each population.
-    for i in range(population_size):
+    for individual in range(population_size):
         solution = Solution(dict())
+        available_units = units.copy()
+
         # Generate a random exam schedule for each day.
         for day in EXAM_DAYS:
-            available_units = units.copy()
             exam_schedule = []
-            for j in range(random.randint(0, len(CLASSROOMS) -1)):
+            random_num_classrooms = random.randint(2, len(CLASSROOMS))
+            
+            if random_num_classrooms > len(available_units):
+                random_num_classrooms = len(available_units)
+
+            #For each day, assign a random morning and afternoon unit & tutor.
+            for classroom in range(random_num_classrooms): 
+                if len(available_units) == 0:
+                    break
                 exam_room = generate_exam_room(available_units, tutors)
                 exam_schedule.append(exam_room)
-                
                 if exam_room.morning in available_units:
                     available_units.remove(exam_room.morning)
                 if exam_room.afternoon in available_units:
                     available_units.remove(exam_room.afternoon)
-
             solution.days[day] = exam_schedule
-        
         # Append the schedule to the new population.
         new_population.append(solution)
 
     return new_population
 
+
+# pick a random number of rooms
+# assign an exam for each room
 
 def hard_constraint_all_units(solution, units):
     """
@@ -170,18 +188,19 @@ def hard_constraint_all_units(solution, units):
             exam_list.append(_class.afternoon)
 
     # Storing each unique unit code.
-    exam_codes = set([unit[0] for unit in units])
+    exam_codes = set([unit for unit in units])
 
     # Number of missing exams in exam_list.
     missing = len(exam_codes.difference(exam_list))
+    missing_exams = exam_codes.difference(exam_list)
 
     # Constraint violated if an exam isn't scheduled.
     if missing > 0:
         valid = False
     
-    num = (1 / (1 + missing))
+    score = 1 / (1 + missing) * 10
 
-    return num, valid
+    return score, valid
 
 
 def hard_constraint_unit_count(unit_allocation):
@@ -219,6 +238,7 @@ def hard_constraint_exam_clash(solution, unit_allocation):
     """
     Hard constraint.
     A student cannot appear in more than one exam at a time.
+    :return: Fitness score and valid.
     """
 
     valid = True
@@ -253,7 +273,10 @@ def hard_constraint_exam_clash(solution, unit_allocation):
     # Invalid exam timetable if there's a timeslot clash.
     if timeslot_clashes > 0:
         valid = False
-    return 1 / (1 + timeslot_clashes), valid
+
+    score = 1 / (1 + timeslot_clashes) * 10
+
+    return score, valid
 
 
 
@@ -295,14 +318,15 @@ def hard_constraint_tutor_clash(solution):
     if clashes > 0:
         valid = False
     
-    num = (1 / (1 + clashes))
+    score = (1 / (1 + clashes) * 10)
 
-    return num, valid
+    return score, valid
 
 
 def hard_constraint_duplicate_exams(solution):
     """
-    Not part of hard constraint list, but it stops duplicate exams.
+    Hard constraint.
+    Not part of hard constraint list, but it counts duplicate exams.
     :return: The score of the constraint and valid.
     """
     exam_list = []
@@ -322,26 +346,101 @@ def hard_constraint_duplicate_exams(solution):
     if duplicates > 1:
         valid = False
 
-    return 1 / (1 + duplicates), valid
+    score = 1 / (1 + duplicates) * 10
+    return score, valid
+
+def soft_constraint_two_exams(solution, unit_allocation):
+    """
+    Soft constraint.
+    A Student should not sit in more than one exam consecutively in a day. 
+    :return: Fitness score and valid.
+    """
+
+    valid = True
+    # Storing the number of clashes in a day.
+    exam_count = defaultdict(int)
+
+    # For each student in each unit on each day.
+    for day in solution.days:
+        for student in unit_allocation:
+            # Count the number of exams for the student on the current day.
+            exam_count[student.name] += count_student_exams(solution, student, day)
+
+    consecutive_exams = 0
+    for name, count in exam_count.items():
+        if count > 1:
+                # Add 1 to timeslot_clashes if true.
+                consecutive_exams += 1
+
+    # Invalid exam timetable if there's a timeslot clash.
+    if consecutive_exams > 0:
+        valid = False
+
+    score = 1 / (1 + consecutive_exams) * 2
+
+    return score, valid
 
 
-def hard_constraints_check(solution, units, unit_allocation):
+def soft_constraint_invigilation_duties(solution, units, tutors):
     """
-    Checks if all hard constraints are satisfied
+    Soft constraint.
+    Tutors should have an equal number of invigilation duties.
     """
+    # Counting the number of clashes.
+    invigilator_list = []
+    desired_average = round(len(units) / len(tutors), 2)
+    valid = True
+
+    # Looping through each exam day.
+    for day in solution.days:
+        unit_list = solution.days[day]
+    
+        # Store the assigned tutors in the morning and afternoon lists.
+        for unit in unit_list:
+            invigilator_list.extend([unit.tutor_morning, unit.tutor_afternoon])
+        
+        # Count the number of times each tutor is in the lists.
+    invigilator_duty_count = Counter(invigilator_list)
+    
+    sum_of_duties = 0
+    for duties in invigilator_duty_count.values():
+       sum_of_duties += duties
+
+    average = sum_of_duties / len(invigilator_duty_count)
+    rounded_average = round(average, 2)
+
+    if rounded_average != desired_average:
+        valid = False
+    
+    absolute = abs(rounded_average - desired_average)
+    score = 1 / (1 + absolute) * 2
+
+    return score, valid
+
+
+def constraints_check(solution, units, unit_allocation, tutors):
+    """
+    Checks if all hard and soft constraints are satisfied.
+    :return: If true, the genetic algorithm will return the solution.
+    """
+    unit_count = hard_constraint_unit_count(unit_allocation)        
     au_score, all_units = hard_constraint_all_units(solution, units)
     de_score, duplicate_exams = hard_constraint_duplicate_exams(solution)
     ec_score, exam_clash = hard_constraint_exam_clash(solution, unit_allocation)
     tc_score, tutor_clash = hard_constraint_tutor_clash(solution)
-    unit_count = hard_constraint_unit_count(unit_allocation)
+    ce_score, consecutive_exams = soft_constraint_two_exams(solution, unit_allocation)
+    ei_score, equal_invigilators = soft_constraint_invigilation_duties(solution, units, tutors)
+    
 
-    if all_units and duplicate_exams and exam_clash and tutor_clash and unit_count:
+    print(f"All Units: {all_units}, Duplicate Exams: {duplicate_exams}, Exam_Clash: {exam_clash}, Tutor Clash: {tutor_clash}, Unit Count: {unit_count}, Consecutive Exams: {consecutive_exams}, Equal Invigilators: {equal_invigilators}")
+
+    if all_units and duplicate_exams and exam_clash and tutor_clash and unit_count and consecutive_exams and equal_invigilators:
         return True
 
     return False
 
 
-def calculate_fitness(population, units, unit_allocation):
+def calculate_fitness(population, units, unit_allocation, tutors):
     """
     Calculate fitness score.
     """
@@ -350,8 +449,10 @@ def calculate_fitness(population, units, unit_allocation):
         de_score, duplicate_exams = hard_constraint_duplicate_exams(solution)
         ec_score, exam_clash = hard_constraint_exam_clash(solution, unit_allocation)
         tc_score, tutor_clash = hard_constraint_tutor_clash(solution)
+        ce_score, consecutive_exams = soft_constraint_two_exams(solution, unit_allocation)
+        ei_score, equal_invigilators = soft_constraint_invigilation_duties(solution, units, tutors)
 
-        fitness = au_score + de_score + ec_score + tc_score         
+        fitness = au_score + de_score + ec_score + tc_score + ce_score + ei_score        
         solution.fitness = fitness                               
 
     return population
@@ -441,7 +542,6 @@ def genetic_algorithm(population_size, max_generations, crossover_probability, u
     best_solution = None
     previous_best = None
     stagnant = 50
-    reset_count = 0
 
     # Generate population.
     population = [generate_population(population_size, units, tutors)]
@@ -450,12 +550,12 @@ def genetic_algorithm(population_size, max_generations, crossover_probability, u
     for i in range(max_generations):
 
         # Calculate the fitness of each solution.
-        population_fitness = calculate_fitness(population[0], units, unit_allocation)
+        population_fitness = calculate_fitness(population[0], units, unit_allocation, tutors)
 
         # Selection and crossover.
         parents = selection(deepcopy(population_fitness))
         crossover_population = apply_crossover(parents, crossover_probability)
-        calculate_fitness(crossover_population, units, unit_allocation)
+        calculate_fitness(crossover_population, units, unit_allocation, tutors)
 
         # Get solution with highest fitness and assign it as best_solution.
         solution1, _ = elitism(crossover_population)
@@ -474,10 +574,10 @@ def genetic_algorithm(population_size, max_generations, crossover_probability, u
         
         # Set the current solution to pthe previous best to check stagnation 
         previous_best = deepcopy(best_solution.fitness)
-        print(f"Generation: {i+1} Current fitness: {best_solution.fitness} Stagnant: {stagnant}")
+        print(f"\nGeneration: {i+1} Current fitness: {best_solution.fitness} Stagnant: {stagnant}")
         
         # Check if all hard_constrats are fulfilled.
-        if hard_constraints_check(best_solution, units, unit_allocation):
+        if constraints_check(best_solution, units, unit_allocation, tutors):
             print("All hard constraints satisfied")
             return best_solution
         
